858e02213306097ca191a232ef9e428162a83b1b
Ömer Said BULDUK <omersaidbuldukk@gmail.com>
2022-06-11 17:50:01 +0300

    finished

--- app_localStorage.js
+++ app_localStorage.js
@@ -20,7 +20,7 @@ function createListElement(todo) {
   //? her bir todo objesini destructure yaptık
   const { id, content, isDone } = todo;
   todoUl.innerHTML += `
-    <li class=${isDone && 'checked'}>
+    <li id=${id} class=${isDone ? 'checked' : ''}>
       <i class="fa fa-check"></i>
       <p>${content}</p>
       <i class="fa fa-trash"></i>
@@ -63,15 +63,32 @@ todoInput.addEventListener('keydown', (e) => {
 
 //? delete ve ok butonları için event tanimlamasi
 todoUl.addEventListener('click', (e) => {
+  const id = e.target.parentElement.getAttribute('id');
   //!Event, sil butonlarının birisinden geldi ise
   if (e.target.classList.contains('fa-trash')) {
-    //? ilgili li elementini silerek DOM'u güncelle
+    //? Dizinin ilgili elementini sildi
+    todos = todos.filter((todo) => todo.id != id);
+
+    //?todos dizisinin son halini localStorage'e sakla
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    //?DOM'daki ilgili li elementini sil
     e.target.parentElement.remove();
   }
 
   //!Event, silme veya ok butonlarından geldi ise
   if (e.target.classList.contains('fa-check')) {
-    //? ilgili li elementinde checked adinda bir class varsa bunu sil
+    // todos dizisindeki ilgili elementin isDone kismini güncelle
+    todos.map((todo, index) => {
+      if (todo.id == id) {
+        todos[index].isDone = !todos[index].isDone;
+      }
+    });
+
+    //?todos dizisinin son halini localStorage'e sakla
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    //? ilgili li elementinde checked adinda bir class varsa bunu sil (DOM)
     if (e.target.parentElement.classList.contains('checked')) {
       e.target.parentElement.classList.remove('checked');
     } else {
